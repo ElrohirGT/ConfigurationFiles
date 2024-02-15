811d8e724ff533c146e9cdddcae542d1c2734b03
ElrohirGT <elrohirgt@gmail.com>
2024-02-15 14:05:39 -0600

    feat: Added a install script for restoring system

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo -e "\e[32m Copying files to /etc/nixos/\e[0m" \
+	&& sudo cp nixos/** /etc/nixos/ \
+	&& echo -e "\e[32m Rebuilding system on boot...\e[0m"\
+	&& sudo nixos-rebuild boot \
+	&& echo -e "\e[32m Building home manager configuration...\e[0m"\
+	&& nix run home-manager/master -- switch \
+	&& reboot
