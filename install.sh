7df7b0fe2779d6007d35521d82281f41c00f6293
ElrohirGT <elrohirgt@gmail.com>
2024-03-10 10:32:49 -0600

    feat: Added flake to NixOS config

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-echo -e "\e[32m Copying files to /etc/nixos/\e[0m" \
-	&& sudo cp nixos/** /etc/nixos/ \
-	&& (rm -r ~/.config/; cp -r . ~/.config/) \
-	&& echo -e "\e[32m Rebuilding system on boot...\e[0m"\
-	&& sudo nixos-rebuild boot \
-	&& echo -e "\e[32m Building home manager configuration...\e[0m"\
-	&& nix run home-manager/master -- switch \
-	&& reboot
+set -e # Exit shell when some command fails
+
+echo -e "\e[32m Copying files to /etc/nixos/\e[0m" 
+sudo cp nixos/** /etc/nixos/
+(rm -r ~/.config/; cp -r . ~/.config/)
+echo -e "\e[32m Rebuilding system on boot...\e[0m"
+sudo nixos-rebuild boot
+echo -e "\e[32m Building home manager configuration...\e[0m"
+nix run home-manager/master -- switch
+reboot
