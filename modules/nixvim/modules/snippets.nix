ed26f519c2a5b2849798f40fea84d1263af2480a
ElrohirGT <elrohirgt@gmail.com>
2024-03-28 08:52:36 -0600

    feat: Added LuaSnip configuration to cmp engine

--- modules/nixvim/modules/snippets.nix
+++ modules/nixvim/modules/snippets.nix
@@ -6,4 +6,7 @@
       store_selection_keys = "<Tab>";
     };
   };
+
+  # Setting up the completion engine to use luasnip
+  plugins.cmp.settings.snippet.expand = ''function(args) require('luasnip').lsp_expand(args.body) end'';
 }
