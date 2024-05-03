fd1d5505d96faf5ed234876b75848d84a5ef1773
ElrohirGT <elrohirgt@gmail.com>
2024-05-03 16:04:43 -0600

    fix: updated vim-doge plugin

--- modules/nixvim/modules/doge.nix
+++ modules/nixvim/modules/doge.nix
@@ -3,16 +3,19 @@
     (
       pkgs.vimUtils.buildVimPlugin {
         name = "doge";
-        version = "v4.6.3";
+        version = "v4.7.0";
         src = pkgs.fetchFromGitHub {
           owner = "kkoomen";
           repo = "vim-doge";
           # Versión 4.6.3
           # https://github.com/kkoomen/vim-doge/releases/tag/v4.6.3
-          rev = "622736ca29ecd6e2720623696d48179c4da430ac";
-          hash = "sha256-avWfs84aOuSV9yX6sOwAj1NOzkGfjXM7S6Wr4W5rVtA=";
+          rev = "ca6f40e7d1262ad0cf13e8cf7887fe7ae05c9949";
+          hash = "sha256-JuLacXfF94/AJ2waCN9KFClGPsa4WB4mUAXVuozW22o=";
         };
       }
     )
   ];
+  extraConfigLua = ''
+    vim.g.doge_install_path = '/home/elrohirgt/vim-doge-bin/'
+  '';
 }
