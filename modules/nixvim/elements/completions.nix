1590ef0ccf4868624f7b3e57ec4718106f2e153c
ElrohirGT <elrohirgt@gmail.com>
2024-06-05 15:06:46 -0600

    fix: Added deleted completion sources

--- modules/nixvim/elements/completions.nix
+++ modules/nixvim/elements/completions.nix
@@ -1,27 +1,39 @@
-{config, ...}: {
-  config = {
-    plugins = {
-      lspkind.enable = true;
+{
+  plugins = {
+    lspkind.enable = true;
 
-      # Completions framework and sources
-      cmp = {
-        enable = true;
+    # Completions framework and sources
+    cmp = {
+      enable = true;
 
-        autoEnableSources = true;
-        settings = {
-          mapping = {
-            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+      settings = {
+        mapping = {
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
 
-            # Manually trigger a completion from nvim-cmp
-            "<C-Space>" = "cmp.mapping.complete()";
-            "<C-e>" = "cmp.mapping.close()";
-            "<C-n>" = "cmp.mapping.select_next_item()";
-            "<C-p>" = "cmp.mapping.select_prev_item()";
-            "<C-y>" = "cmp.mapping.confirm({ select = true })";
-          };
+          # Manually trigger a completion from nvim-cmp
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-e>" = "cmp.mapping.close()";
+          "<C-n>" = "cmp.mapping.select_next_item()";
+          "<C-p>" = "cmp.mapping.select_prev_item()";
+          "<C-y>" = "cmp.mapping.confirm({ select = true })";
         };
       };
+      # Auto installs sources listed below if recognized by nixvim.
+      autoEnableSources = true;
+      sources = [
+        # LSP and Treessitter on top
+        {name = "nvim_lsp";}
+        {name = "nvim_lsp_signature_help";}
+        {name = "treesitter";}
+
+        {name = "path";}
+        {name = "calc";}
+        {name = "buffer";}
+
+        # Completion for neovim Lua API.
+        {name = "nvim_lua";}
+      ];
     };
   };
 }
