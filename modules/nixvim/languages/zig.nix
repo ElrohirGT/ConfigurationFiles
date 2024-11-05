465e13469e4f0b5d4923d48c0ccfb843f3b82578
ElrohirGT <elrohirgt@gmail.com>
2024-11-05 14:37:13 -0600

    feat: Added zig

--- modules/nixvim/languages/zig.nix
+++ modules/nixvim/languages/zig.nix
@@ -1,3 +1,11 @@
-{
+{pkgs, ...}: {
   plugins.zig.enable = true;
+
+  plugins.lsp = {
+    servers.zls.enable = true;
+  };
+
+  plugins.treesitter = {
+    grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [zig];
+  };
 }
