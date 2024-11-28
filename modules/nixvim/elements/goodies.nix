3462783d1f1eadf54eb37b15ba02980e4c2a9f93
ElrohirGT <elrohirgt@gmail.com>
2024-11-28 16:35:42 -0600

    feat: Added faster config

--- modules/nixvim/elements/goodies.nix
+++ modules/nixvim/elements/goodies.nix
@@ -1,8 +1,13 @@
-{pkgs, ...}: {
+{pkgs, ...}: 
+let 
+fasterPlugin = import ../plugins/faster.plugin.nix pkgs;
+in{
   extraPlugins = with pkgs; [
     # You'll need to install nerdfont symbol font.
     vimPlugins.nvim-web-devicons
     vimPlugins.dropbar-nvim
-    (import ../plugins/faster.plugin.nix pkgs)
+    (fasterPlugin.package)
   ];
+
+	extraConfigLua = fasterPlugin.luaConfig;
 }
--- modules/nixvim/plugins/faster.plugin.nix
+++ modules/nixvim/plugins/faster.plugin.nix
@@ -1,4 +1,6 @@
 {pkgs, ...}:
+{
+	package = 
 pkgs.vimUtils.buildVimPlugin {
   name = "Faster.nvim";
   src = pkgs.fetchFromGitHub {
@@ -7,4 +9,8 @@ pkgs.vimUtils.buildVimPlugin {
     rev = "ee58dbb46e5d16b72cdfd0cd246a3b1a33ec2487";
     hash = "sha256-p5T8gl4fBfMu7UQsekOFUM7/9ow22Zwt2pfNXoobpyc=";
   };
+};
+luaConfig = ''
+require('faster').setup()
+'';
 }
