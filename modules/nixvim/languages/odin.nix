fdd604ea048f7ad2490ba84cf5ed305df515422b
ElrohirGT <elrohirgt@gmail.com>
2025-08-04 16:40:22 -0600

    feat: Added OdinLSP

--- modules/nixvim/languages/odin.nix
+++ modules/nixvim/languages/odin.nix
@@ -1,4 +1,8 @@
 {pkgs, ...}: {
+  plugins.lsp = {
+    servers.ols.enable = true;
+  };
+
   plugins.treesitter = {
     grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
       odin
