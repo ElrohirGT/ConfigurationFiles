448fead9ba6e70ef2d3ff3edff9f1f4c30baa1cb
ElrohirGT <elrohirgt@gmail.com>
2025-08-16 15:55:21 -0600

    feat: Added parameter to template

--- modules/templates/basic/flake.nix
+++ modules/templates/basic/flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = {nixpkgs}: let
+  outputs = {nixpkgs, ...}: let
     # System types to support.
     supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
 
