be20643e1f1792f15b2c4eb30c67a8b1d8de14ee
ElrohirGT <elrohirgt@gmail.com>
2026-04-09 20:29:01 -0600

    feat: Improved typst setup

--- modules/nixvim/languages/typst.nix
+++ modules/nixvim/languages/typst.nix
@@ -1,10 +1,19 @@
 {pkgs, ...}: {
-  # lsp = {
-  #   servers.yamlls.enable = true;
-  # };
+  lsp = {
+    servers.tinymist.enable = true;
+  };
 
   plugins.typst-vim.enable = true;
-  plugins.typst-preview.enable = true;
+  plugins.typst-preview = {
+    enable = true;
+    settings = {
+      open_cmd = "firefox %s -class typst-preview";
+      # dependencies_bin = {
+      #   tinymist = "tinymist";
+      #   websocat = "websocat";
+      # };
+    };
+  };
 
   plugins.treesitter = {
     grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
