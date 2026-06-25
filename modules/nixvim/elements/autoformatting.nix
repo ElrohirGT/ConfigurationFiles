b83d90d668f5625ff0dc7985c088a41aacb973d8
ElrohirGT <elrohirgt@gmail.com>
2026-06-25 16:35:43 -0600

    feat: Added autoformatting for header files

--- modules/nixvim/elements/autoformatting.nix
+++ modules/nixvim/elements/autoformatting.nix
@@ -93,7 +93,7 @@
     (genCmd ["*.zig"] "!zig fmt '%'")
 
     # C files
-    (genCmd ["*.c" "*.cpp"] "!clang-format -i '%'")
+    (genCmd ["*.c" "*.h" "*.cpp"] "!clang-format -i '%'")
 
     # Go files
     (genCmd ["*.go"] "!gofmt -w '%'")
