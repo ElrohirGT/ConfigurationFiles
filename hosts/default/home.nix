8aaa3078559eca0e0ff80325313517d021ee0573
ElrohirGT <elrohirgt@gmail.com>
2026-07-14 18:42:30 -0600

    fix: tabs

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -197,13 +197,13 @@ in
         extraConfig = ''
           return {
           	color_scheme = 'Helios (base16)',
-          		enable_tab_bar = false,
-          		window_padding = {
-          			left = 0,
-          			right = 0,
-          			top = 0,
-          			bottom = 0,
-          		},
+          	hide_tab_bar_if_only_one_tab = true,
+          	window_padding = {
+          		left = 0,
+          		right = 0,
+          		top = 0,
+          		bottom = 0,
+          	},
           }
         '';
       };
