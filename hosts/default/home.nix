796990d2b1002236cdaf22c83e6734e6cf7ea2f6
ElrohirGT <elrohirgt@gmail.com>
2026-07-14 18:28:38 -0600

    fix: wezterm config

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -195,14 +195,16 @@ in
       programs.wezterm = {
         enable = true;
         extraConfig = ''
-               config.color_scheme = 'FirefoxDev'
-               config.enable_tab_bar = false
-             	config.window_padding = {
-          left = 0,
-          right = 0,
-          top = 0,
-          bottom = 0,
-             }
+          return {
+          	color_scheme = 'FirefoxDev',
+          		enable_tab_bar = false,
+          		window_padding = {
+          			left = 0,
+          			right = 0,
+          			top = 0,
+          			bottom = 0,
+          		},
+          }
         '';
       };
 
