24fb06208f2923c533b15c063ecd5180b839f14f
ElrohirGT <elrohirgt@gmail.com>
2026-09-23 16:39:31 -0600

    fix: config

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -299,7 +299,7 @@ in
 
       programs.superfile = {
         enable = true;
-        hotkeys = ''
+        hotkeys = {
           ##############################################
           #                                            #
           #         Superfile vim-like Hotkeys         #
@@ -319,61 +319,61 @@ in
           # Note: These hotkeys should be unique.
 
           #-- Basic Actions
-          confirm = ["enter", ""]
-          quit = ["ctrl+c", ""] # a.k.a. "theprimeagen troller"
-          cd_quit = ["Q", ""]
+          confirm = ["enter" ""];
+          quit = ["ctrl+c" ""]; # a.k.a. "theprimeagen troller"
+          cd_quit = ["Q" ""];
 
           #-- Navigation
-          list_up = ["k", ""]
-          list_down = ["j", ""]
-          page_up = ["pgup",""]
-          page_down = ["pgdown",""]
+          list_up = ["k" ""];
+          list_down = ["j" ""];
+          page_up = ["pgup" ""];
+          page_down = ["pgdown" ""];
 
           #-- File Panel Controls
-          create_new_file_panel = ["n", ""]
-          close_file_panel = ["q", ""]
-          next_file_panel = ["tab", ""]
-          previous_file_panel = ["shift+tab", ""]
-          split_file_panel = ["N", ""]
-          toggle_file_preview_panel = ["f", ""]
-          open_sort_options_menu = ["o", ""]
-          toggle_reverse_sort = ["R", ""]
+          create_new_file_panel = ["n" ""];
+          close_file_panel = ["q" ""];
+          next_file_panel = ["tab" ""];
+          previous_file_panel = ["shift+tab" ""];
+          split_file_panel = ["N" ""];
+          toggle_file_preview_panel = ["f" ""];
+          open_sort_options_menu = ["o" ""];
+          toggle_reverse_sort = ["R" ""];
 
           #-- Focus Manipulation
-          focus_on_process_bar = ["ctrl+p", ""]
-          focus_on_sidebar = ["ctrl+s", ""]
-          focus_on_metadata = ["ctrl+d", ""]
+          focus_on_process_bar = ["ctrl+p" ""];
+          focus_on_sidebar = ["ctrl+s" ""];
+          focus_on_metadata = ["ctrl+d" ""];
 
           #-- File/Dir Creation/Renaming
-          file_panel_item_create = ["a", ""]
-          file_panel_item_rename = ["r", ""]
+          file_panel_item_create = ["a" ""];
+          file_panel_item_rename = ["r" ""];
 
           #-- Main File Operations
-          copy_items = ["y", ""]
-          cut_items = ["x", ""]
-          paste_items = ["p", ""]
-          delete_items = ["d", ""]
-          permanently_delete_items = ["D", ""]
+          copy_items = ["y" ""];
+          cut_items = ["x" ""];
+          paste_items = ["p" ""];
+          delete_items = ["d" ""];
+          permanently_delete_items = ["D" ""];
 
           #-- Archive Manipulation
-          extract_file = ["ctrl+e", ""]
-          compress_file = ["ctrl+a", ""]
+          extract_file = ["ctrl+e" ""];
+          compress_file = ["ctrl+a" ""];
 
           #-- Editor Actions
-          open_file_with_editor = ["e", ""]
-          open_current_directory_with_editor = ["E", ""]
+          open_file_with_editor = ["e" ""];
+          open_current_directory_with_editor = ["E" ""];
 
           #-- Other Actions
-          pinned_directory = ["P", ""]
-          toggle_dot_file = [".", ""]
-          change_panel_mode = ["m", ""]
-          open_help_menu = ["?", ""]
-          open_spf_prompt = [">", ""]
-          open_command_line = [":", ""]
-          open_zoxide = ["z", ""]
-          copy_path = ["Y", ""]
-          copy_present_working_directory = ["c", ""]
-          toggle_footer = ["ctrl+f", ""]
+          pinned_directory = ["P" ""];
+          toggle_dot_file = ["." ""];
+          change_panel_mode = ["m" ""];
+          open_help_menu = ["?" ""];
+          open_spf_prompt = [">" ""];
+          open_command_line = [":" ""];
+          open_zoxide = ["z" ""];
+          copy_path = ["Y" ""];
+          copy_present_working_directory = ["c" ""];
+          toggle_footer = ["ctrl+f" ""];
 
           ###############################################################################
           #                                Typing hotkeys                               #
@@ -381,8 +381,8 @@ in
 
           # Note: These hotkeys can override all hotkeys.
 
-          confirm_typing = ["enter", ""]
-          cancel_typing = ["esc", ""]
+          confirm_typing = ["enter" ""];
+          cancel_typing = ["esc" ""];
 
           ###############################################################################
           #                            Mode-Specific Hotkeys                            #
@@ -392,14 +392,14 @@ in
           # hotkeys.
 
           #-- Normal Mode Actions
-          parent_directory = ["-", ""]
-          search_bar = ["/", ""]
+          parent_directory = ["-" ""];
+          search_bar = ["/" ""];
 
           #-- Selection Mode Actions
-          file_panel_select_mode_items_select_down = ["J", ""]
-          file_panel_select_mode_items_select_up = ["K", ""]
-          file_panel_select_all_items = ["A", ""]
-        '';
+          file_panel_select_mode_items_select_down = ["J" ""];
+          file_panel_select_mode_items_select_up = ["K" ""];
+          file_panel_select_all_items = ["A" ""];
+        };
       };
 
       # Tmux config
